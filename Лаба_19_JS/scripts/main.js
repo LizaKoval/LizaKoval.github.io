f48be4597df76c519ce4d2caac28a59d20b57e6b
LizaKoval <63054459+LizaKoval@users.noreply.github.com>
2020-11-16 21:40:14 +0300

    Add files via upload

---
+++ Лаба_19_JS/scripts/main.js
@@ -0,0 +1,36 @@
+let arr = ['Happy', 2, 'c', 'you'];
+
+alert('первоначальный массив:');
+
+alert(arr);
+
+alert(arr[0]);
+
+alert(arr[1]);
+
+arr[2]='meet';
+
+alert(arr[2]);
+
+arr.unshift('So');
+
+arr.push('here');
+
+alert('изменённый массив:');
+
+alert(arr);
+
+alert(arr.length)
+
+var str = 'Every man in the world! Every woman on earth!';
+
+console.log(str);
+
+var re = /(wo)?man/gi;
+
+re.compile(re);
+
+var newstring = str.replace(re, 'person');
+
+console.log(newstring);
+
